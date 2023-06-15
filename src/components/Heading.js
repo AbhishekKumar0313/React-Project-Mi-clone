59559e84a841b0d7796b268bcda734511cc306e8
Abhishek Kumar <abhi.keshri0313@gmail.com>
2023-06-15 13:37:09 +0530

    Mi-clone-React

---
+++ src/components/Heading.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../Styles/starpro.css"
+const  Heading=({text})=> {
+  return (
+    <div className='Heading'>
+      <div></div>
+      <p>{text}</p>
+      <div></div>
+    </div>
+  )
+}
+
+export default  Heading
